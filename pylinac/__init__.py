a032afdc6b1d8265f6ac1e7e6361e2c723a036d4
Sharaf M. Mansour <mmgalal2002@gmail.com>
2026-09-13 01:28:55 +0300

    Update __init__.py

--- pylinac/__init__.py
+++ pylinac/__init__.py
@@ -21,6 +21,7 @@ from .field_analysis import (
     Protocol,
 )
 from .field_profile_analysis import FieldProfileAnalysis
+from .ge_ct_qa import GECTQA
 from .helios import GEHeliosCTDaily
 from .log_analyzer import Dynalog, MachineLogs, TrajectoryLog, load_log
 from .picketfence import PicketFence  # must be after log analyzer
